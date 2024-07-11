fd1a4be22c8a50d5d8c8b5cc99097b59f0d20481
JerryKiki <gasi758@gmail.com>
2024-07-11 11:02:55 +0900

    0711문제í풀이1

--- mySqlPracCodeSave.sql
+++ mySqlPracCodeSave.sql
@@ -501,3 +501,155 @@ FROM t_order
          INNER JOIN t_product
                     ON t_order.productNo = t_product.id
 WHERE t_user.userName = '소지섭';
+
+######################################################
+
+#0711 SUM, MAX, MIN, COUNT 문제풀이
+
+# a6 DB 삭제/생성/선택
+DROP DATABASE IF EXISTS `a6`;
+CREATE DATABASE a6;
+USE a6;
+# 부서(홍보, 기획)
+CREATE TABLE dept(
+                     id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                     regDate DATETIME,
+                     `name` CHAR(100)
+);
+INSERT INTO dept
+SET regDate = NOW(),
+	`name` = '홍보';
+INSERT INTO dept
+SET regDate = NOW(),
+	`name` = '기획';
+SELECT * FROM dept;
+# 사원(홍길동/홍보/5000만원, 홍길순/홍보/6000만원, 임꺽정/기획/4000만원)
+CREATE TABLE emp(
+                    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    regDate DATETIME,
+                    `name` CHAR(100),
+                    deptId INT(10),
+                    wage INT(100) UNSIGNED
+);
+INSERT INTO emp
+SET regDate = NOW(),
+	`name` = '홍길동',
+	deptId = 1,
+	wage = 5000;
+INSERT INTO emp
+SET regDate = NOW(),
+	`name` = '홍길순',
+	deptId = 1,
+	wage = 6000;
+INSERT INTO emp
+SET regDate = NOW(),
+	`name` = '임꺽정',
+	deptId = 2,
+	wage = 4000;
+SELECT * FROM emp;
+# 사원 수 출력
+SELECT COUNT(*) AS '사원 수'
+FROM emp;
+# 가장 큰 사원 번호 출력
+SELECT MAX(id) AS '가장 큰 사원 번호'
+FROM emp;
+# 가장 고액 연봉
+SELECT MAX(wage) AS '가장 고액 연봉'
+FROM emp;
+# 가장 저액 연봉
+SELECT MIN(wage) AS '가장 저액 연봉'
+FROM emp;
+# 회사에서 1년 고정 지출(인건비)
+SELECT CONCAT(SUM(wage), '만원') AS '회사 고정 지출'
+FROM emp;
+# 부서별, 1년 고정 지출(인건비)
+SELECT dept.`name`, CONCAT(SUM(emp.wage), '만원') AS '부서별 고정 지출'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId;
+# 부서별, 최고연봉
+SELECT dept.`name`, CONCAT(MAX(emp.wage), '만원') AS '부서별 최고 연봉'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId;
+# 부서별, 최저연봉
+SELECT dept.`name`, CONCAT(MIN(emp.wage), '만원') AS '부서별 최저 연봉'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId;
+# 부서별, 평균연봉
+SELECT dept.`name`, CONCAT(ROUND(AVG(emp.wage)), '만원') AS '부서별 평균 연봉'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId;
+# 부서별, 부서명, 사원리스트, 평균연봉, 최고연봉, 최소연봉, 사원수
+## V1(조인 안한 버전)
+SELECT
+    deptId,
+    GROUP_CONCAT(`name`) AS '사원리스트',
+        CONCAT(ROUND(AVG(wage)), '만원') AS '평균연봉',
+        CONCAT(MAX(wage), '만원') AS '최고연봉',
+        CONCAT(MIN(wage), '만원') AS '최소연봉',
+        CONCAT(COUNT(id), '명') AS '사원 수'
+FROM emp
+GROUP BY deptId;
+## V2(조인해서 부서명까지 나오는 버전)
+SELECT
+    dept.`name` AS '부서명',
+        GROUP_CONCAT(emp.`name`) AS '사원리스트',
+        CONCAT(ROUND(AVG(emp.wage)), '만원') AS '평균연봉',
+        CONCAT(MAX(emp.wage), '만원') AS '최고연봉',
+        CONCAT(MIN(emp.wage), '만원') AS '최소연봉',
+        CONCAT(COUNT(emp.id), '명') AS '사원 수'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId;
+## V3(V2에서 평균연봉이 5000이상인 부서로 추리기)
+SELECT
+    dept.`name` AS '부서명',
+        GROUP_CONCAT(emp.`name`) AS '사원리스트',
+        CONCAT(ROUND(AVG(emp.wage)), '만원') AS '평균연봉',
+        CONCAT(MAX(emp.wage), '만원') AS '최고연봉',
+        CONCAT(MIN(emp.wage), '만원') AS '최소연봉',
+        CONCAT(COUNT(emp.id), '명') AS '사원 수'
+FROM emp
+         INNER JOIN dept
+                    ON emp.deptId = dept.id
+GROUP BY deptId
+HAVING AVG(emp.wage) >= 5000;
+## V4(V3에서 HAVING 없이 서브쿼리로 수행)
+### HINT, UNION을 이용한 서브쿼리
+# SELECT *
+                # FROM (
+                           #     select 1 AS id
+#     union
+#     select 2
+#     UNION
+#     select 3
+# ) AS A
+SELECT
+    `부서명`,
+    `사원리스트`,
+    CONCAT(`평균연봉`, '만원') AS '평균연봉',
+        `최고연봉`,
+    `최소연봉`,
+    `사원 수`
+FROM (
+         SELECT
+             dept.`name` AS `부서명`,
+             GROUP_CONCAT(emp.`name`) AS `사원리스트`,
+             ROUND(AVG(emp.wage)) AS `평균연봉`,
+             CONCAT(MAX(emp.wage), '만원') AS `최고연봉`,
+             CONCAT(MIN(emp.wage), '만원') AS `최소연봉`,
+             CONCAT(COUNT(emp.id), '명') AS `사원 수`
+         FROM emp
+                  INNER JOIN dept
+                             ON emp.deptId = dept.id
+         GROUP BY deptId
+     ) AS a
+WHERE a.`평균연봉` >= 5000;
