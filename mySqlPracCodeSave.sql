5dd9330b4b5d54774b8e320afa4f5b2bbb39169f
JerryKiki <gasi758@gmail.com>
2024-07-15 11:37:12 +0900

    0715 Q27

--- mySqlPracCodeSave.sql
+++ mySqlPracCodeSave.sql
@@ -975,3 +975,193 @@ FROM (
 ) AS DEPT10
 INNER JOIN DEPT
 ON DEPT10.DEPTNO = DEPT.DEPTNO;
+
+##################################################
+
+#0715 문제27 풀이
+
+# 데이터베이스 a4가 존재하면 삭제
+DROP DATABASE IF EXISTS `a4`;
+
+# 데이터베이스 a4 생성
+CREATE DATABASE `a4`;
+
+# 데이터베이스 a4 선택
+USE `a4`;
+
+# 회원 테이블 생성, loginId, loginPw, `name`
+## 조건 : loginId 칼럼에 UNIQUE INDEX 없이
+CREATE TABLE members(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME,
+	loginId CHAR(100) NOT NULL,
+	loginPw CHAR(100) NOT NULL,
+	`name` CHAR(100) NOT NULL
+);
+
+DESC members;
+
+# 회원 2명 생성
+## 조건 : (loginId = 'user1', loginPw = 'user1', `name` = '홍길동')
+## 조건 : (loginId = 'user2', loginPw = 'user2', `name` = '홍길순')
+INSERT INTO members
+SET regDate = NOW(),
+loginId = 'user1',
+loginPw = 'user1',
+`name` = '홍길동';
+
+INSERT INTO members
+SET regDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '홍길순';
+
+SELECT * FROM members;
+
+# 회원 2배 증가 쿼리만들고 회원이 만명 넘을 때 까지 반복 실행
+## 힌트1 : INSERT INTO `tableName` (col1, col2, col3, col4)
+## 힌트2 : SELECT NOW(), UUID(), 'pw', '아무개'
+INSERT INTO members (regDate, loginId, loginPw, `name`)
+SELECT NOW(), UUID(), 'pw', '아무개'
+FROM members;
+
+# 회원수 확인
+SELECT COUNT(*) AS '회원수'
+FROM members;
+
+# 검색속도 확인
+## 힌트 : SQL_NO_CACHE
+SELECT SQL_NO_CACHE *
+FROM members;
+
+# 유니크 인덱스를 loginID 칼럼에 걸기
+## 설명 : mysql이 loginId의 고속검색을 위한 부가데이터를 자동으로 관리(생성/수정/삭제) 한다.
+## 설명 : 이게 있고 없고가, 특정 상황에서 어마어마한 성능차이를 가져온다.
+## 설명 : 생성된 인덱스의 이름은 기본적으로 칼럼명과 같다.
+ALTER TABLE members ADD UNIQUE INDEX(`loginId`);
+
+# 검색속도 확인, loginId 가 'user1' 인 회원 검색
+SELECT SQL_NO_CACHE *
+FROM members
+WHERE loginId = 'user1';
+
+# 인덱스 삭제, `loginId` 이라는 이름의 인덱스 삭제
+ALTER TABLE members DROP INDEX `loginId`;
+
+# 회원 테이블 삭제
+DROP TABLE members;
+
+# 회원 테이블을 생성하는데, loginId에 uniqueIndex 까지 걸어주세요.
+CREATE TABLE members(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME,
+	loginId CHAR(100) UNIQUE NOT NULL,
+	loginPw CHAR(100) NOT NULL,
+	`name` CHAR(100) NOT NULL
+);
+
+# 회원 2명 생성
+## 조건 : (loginId = 'user1', loginPw = 'user1', `name` = '홍길동')
+## 조건 : (loginId = 'user2', loginPw = 'user2', `name` = '홍길순')
+INSERT INTO members
+SET regDate = NOW(),
+loginId = 'user1',
+loginPw = 'user1',
+`name` = '홍길동';
+
+INSERT INTO members
+SET regDate = NOW(),
+loginId = 'user2',
+loginPw = 'user2',
+`name` = '홍길순';
+
+# 회원수 확인
+SELECT COUNT(*) AS '회원수'
+FROM members;
+
+# 인덱스 쓰는지 확인
+## 힌트 : EXPLAIN SELECT SQL_NO_CACHE * ~
+EXPLAIN SELECT SQL_NO_CACHE *
+FROM members
+WHERE loginId = 'user1';
+
+# 게시물 테이블 생성(title, body, writerName, memberId)
+DROP TABLE articles;
+CREATE TABLE articles(
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	title CHAR(100) NOT NULL,
+	`body` TEXT NOT NULL,
+	writerName CHAR(100),
+	memberId CHAR(100)
+);
+
+DESC articles;
+
+# 1번 회원이 글 1 작성(title = '글 1 제목', `body` = '글 1 내용')
+SELECT * FROM members WHERE id = 1;
+
+INSERT INTO articles
+SET regDate = NOW(),
+title = '글 1 제목',
+`body` = '글 1 내용',
+writerName = '홍길동',
+memberId = 1;
+
+# 2번 회원이 글 2 작성(title = '글 2 제목', `body` = '글 2 내용')
+
+INSERT INTO articles
+SET regDate = NOW(),
+title = '글 2 제목',
+`body` = '글 2 내용',
+writerName = '홍길순',
+memberId = 2;
+
+
+# 1번 회원이 글 3 작성(title = '글 3 제목', `body` = '글 3 내용')
+
+INSERT INTO articles
+SET regDate = NOW(),
+title = '글 3 제목',
+`body` = '글 3 내용',
+writerName = '홍길동',
+memberId = 1;
+
+
+# 전체글 조회
+SELECT * FROM articles;
+
+# 1번회원의 이름변경 홍길동 => 홍길동2
+UPDATE members
+SET `name` = '홍길동2'
+WHERE `name` = '홍길동';
+
+# 전체글 조회, 여전히 게시물 테이블에는 이전 이름이 남아 있음
+SELECT * FROM articles;
+
+# 게시물 테이블에서 writerName 삭제
+ALTER TABLE articles DROP COLUMN `writerName`;
+
+# INNER JOIN 을 통해서 두 테이블을 조회한 결과를 합침, ON 없이
+SELECT * FROM articles
+INNER JOIN members;
+
+# INNER JOIN 을 통해서 두 테이블을 조회한 결과를 합침, 올바른 조인 조건
+SELECT * FROM articles a
+INNER JOIN members m
+ON a.memberId = m.id;
+
+## 힌트 : 이걸로 조인조건을 걸 칼럼 조사
+## SELECT article.id, article.memberId, member.id AS "회원테이블_번호"
+## FROM article
+## INNER JOIN `member`;
+
+SELECT articles.id, articles.memberId, members.id AS "회원테이블_번호"
+FROM articles
+INNER JOIN `members`;
+
+# 조인 완성, ON 까지 추가
+SELECT a.id, a.title, a.memberid, m.`name` FROM articles a
+INNER JOIN members m
+ON a.memberId = m.id
+ORDER BY a.id;
